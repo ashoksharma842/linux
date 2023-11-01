e49625a3cb2a14a60c5fa41acbb39982e22ea17d
Ashok Sharma <ashoksharma842@gmail.com>
2023-11-01 12:44:42 +0530

    Update server.c

--- server.c
+++ server.c
@@ -1,65 +1,64 @@
-// Server side C/C++ program to demonstrate Socket
-// programming
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
-#define PORT 8080
+#include <netdb.h>
+#define PORT "8888"
 int main(int argc, char const* argv[])
 {
-	int server_fd, new_socket;
-	ssize_t valread;
-	struct sockaddr_in address;
-	int opt = 1;
-	socklen_t addrlen = sizeof(address);
-	char buffer[1024] = { 0 };
-	char* hello = "Hello from server";
+    int server_fd, new_socket;
+    ssize_t valread;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+    char buffer[1024] = { 0 };
+    char* hello = "Hello from server";
+    struct addrinfo hints;
+    struct addrinfo *servinfo;
+    char sendbuff[2048] = {0};
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+    getaddrinfo(NULL,PORT,&hints, &servinfo);
+    int count = 0;
+    char zeros[1024] = {0};
 
-	// Creating socket file descriptor
-	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-		perror("socket failed");
-		exit(EXIT_FAILURE);
-	}
+    if ((server_fd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol)) < 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
 
-	// Forcefully attaching socket to the port 8080
-	if (setsockopt(server_fd, SOL_SOCKET,
-				SO_REUSEADDR | SO_REUSEPORT, &opt,
-				sizeof(opt))) {
-		perror("setsockopt");
-		exit(EXIT_FAILURE);
-	}
-	address.sin_family = AF_INET;
-	address.sin_addr.s_addr = INADDR_ANY;
-	address.sin_port = htons(PORT);
-
-	// Forcefully attaching socket to the port 8080
-	if (bind(server_fd, (struct sockaddr*)&address,
-			sizeof(address))
-		< 0) {
-		perror("bind failed");
-		exit(EXIT_FAILURE);
-	}
-	if (listen(server_fd, 3) < 0) {
-		perror("listen");
-		exit(EXIT_FAILURE);
-	}
-	if ((new_socket
-		= accept(server_fd, (struct sockaddr*)&address,
-				&addrlen))
-		< 0) {
-		perror("accept");
-		exit(EXIT_FAILURE);
-	}
-	valread = read(new_socket, buffer, 1024 - 1); // subtract 1 for the null terminator at the end
-	printf("%s\n", buffer);
-	send(new_socket, hello, strlen(hello), 0);
-	printf("Hello message sent\n");
-
-	// closing the connected socket
-	close(new_socket);
-	// closing the listening socket
-	close(server_fd);
-	return 0;
+    if (bind(server_fd, servinfo->ai_addr, servinfo->ai_addrlen) < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+    if (listen(server_fd, 3) < 0) {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+    if ((new_socket = accept(server_fd, (struct sockaddr*)&address, &addrlen)) < 0) {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+    while(1){
+        memset(buffer,0,sizeof(buffer));
+        valread = read(new_socket, buffer, 1023);
+        printf("%s\n",buffer);
+        if(strcmp(buffer,"exit") == 0){
+            printf("exit received\n");
+            close(new_socket);
+            close(server_fd);
+            return 0;
+        }
+        if(memcmp(buffer, zeros,sizeof(buffer))) {
+        memset(sendbuff,0,sizeof(sendbuff));
+        sprintf(sendbuff,"%s rx ", buffer);
+        send(new_socket, sendbuff, sizeof(sendbuff), 0);
+        }
+    }
+    close(new_socket);
+    close(server_fd);
+    return 0;
 }
